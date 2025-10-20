9f67b01fa6253e79ae041fb3ab825222b1f80ff8
Reyzzel <demc.valeria@gmail.com>
2025-10-20 21:17:50 +0300

    Add files via upload

---
+++ encapsulation/TestModifierLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestModifierLibrary
+{
+    internal class A
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("TestModifierLibrary A MethodA");
+        }
+    }
+
+    public class B
+    {
+        public void MethodB()
+        {
+            Console.WriteLine("TestModifierLibrary B MethodB");
+        }
+    }
+}
---
+++ encapsulation/encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestModifierLibrary;
+
+namespace encapsulation
+{
+    class A
+    {
+        protected int a;
+        void MethodA(A a1, B b1)
+        {
+            a1.a = 1;
+            b1.a = 2;
+        }
+    }
+    class B : A
+    {
+        void MethodB(A a1, B b1)
+        {
+            a1.a = 1;
+            this.a = 10;
+            b1.a = 2;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
