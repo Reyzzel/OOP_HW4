667d7b03a3121ca8a7a546dee168d8e76392e7b9
Reyzzel <demc.valeria@gmail.com>
2025-10-20 21:18:30 +0300

    Add files via upload

---
+++ polymorphism/abstract_classes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+public abstract class A
+{
+	public int a;
+	public void Method1()
+	{
+		
+    }
+}
+
+public class Program
+{
+	private static void Main(string[] args)
+	{
+		A classA = new A();
+		Console.ReadKey();
+	}
+}
