c3074c9449e9b4c79d7f300048f5883bdb749f4e
Reyzzel <demc.valeria@gmail.com>
2025-10-20 21:17:00 +0300

    Add files via upload

---
+++ Abstract_Classes/Abstract_Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using Abstract_Classes;
+using System;
+using System.Text;
+
+namespace Abstract_Classes
+{
+    public class A
+    {
+        public void Method1()
+        {
+            Console.WriteLine("class A Method1");
+        }
+    }
+    public static abstract class B : A
+    {
+        public new abstract void Method1();
+    }
+    public class C : B
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("class C Method1");
+        }
+    }
+
+    public class Program
+    {
+        private static void Main()
+        {
+            A a = new A();
+            A b = new A();
+            A c = new A();
+            a.Method1();
+            b.Method1();
+            c.Method1();
+            Console.ReadKey();
+        }
+    }
+}
